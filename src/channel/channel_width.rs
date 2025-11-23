2b71ad902716342a8b1c72b6423153add24ea7c3
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2025-11-23 13:10:20 -0500

    Check EHT/HE Operation IEs for channel width

--- src/channel/channel_width.rs
+++ src/channel/channel_width.rs
@@ -1,152 +1,113 @@
-use crate::Ie;
-use derive_more::{
-    BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Deref, DerefMut, From, Not,
-};
-use enumflags2::{bitflags, BitFlags};
-use std::{convert::From, fmt::Display};
+use std::{collections::HashSet, convert::From, fmt::Display, hash::Hash};
 
-#[bitflags]
-#[derive(Copy, Clone, Debug, PartialEq, Ord, PartialOrd, Eq)]
+use derive_more::{Deref, DerefMut, From};
+use num_enum::TryFromPrimitive;
+
+use crate::{Ie, IeData, ies::ht_operation::SupportedChannelWidths};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Ord, PartialOrd, TryFromPrimitive)]
 #[repr(u8)]
 pub enum ChannelWidth {
-    TwentyMhz = 1 << 0,
-    TwentyTwoMhz = 1 << 1,
-    FortyMhz = 1 << 2,
-    EightyMhz = 1 << 3,
-    EightyPlusEightyMhz = 1 << 4,
-    OneSixtyMhz = 1 << 5,
+    TwentyMhzNoHt,
+    TwentyMhz,
+    FortyMhz,
+    EightyMhz,
+    EightyPlusEightyMhz,
+    OneSixtyMhz,
+    FiveMhzOfdm,
+    TenMhzOfdm,
+    OneMhzOfdm,
+    TwoMhzOfdm,
+    FourMhzOfdm,
+    EightMhzOfdm,
+    SixteenMhzOfdm,
+    ThreeHundredTwentyMhz,
 }
 
 impl From<&[Ie]> for ChannelWidth {
     // From Table 11-24 in IEEE Std 802.11-2016
     fn from(ies: &[Ie]) -> Self {
-        let ht_channel_widths = ies
-            .iter()
-            .find_map(|ie| match ie {
-                Ie::HtOperation(ht_operation) => Some(ht_operation.sta_channel_width()),
-                _ => None,
-            })
-            .unwrap_or(ChannelWidth::TwentyMhz.into());
-
-        let (vht_channel_widths, channel_center_segment_zero, channel_center_segment_one) = ies
-            .iter()
-            .find_map(|ie| match ie {
-                Ie::VhtOperation(vht_operation) => Some((
-                    vht_operation.channel_width(),
-                    vht_operation.channel_center_freq_segment_zero(),
-                    vht_operation.channel_center_freq_segment_one(),
-                )),
-                _ => None,
-            })
-            .unwrap_or((ChannelWidth::TwentyMhz.into(), 0, 0));
-
-        if ht_channel_widths == ChannelWidths::from(ChannelWidth::TwentyMhz) {
-            return ChannelWidth::TwentyMhz;
+        // Check for an EHT Operation element
+        if let Some(eht_operation) = ies.iter().find_map(|ie| match &ie.data {
+            IeData::EhtOperation(eht_operation) => Some(eht_operation),
+            _ => None,
+        }) {
+            if let Some(channel_width) = eht_operation.channel_width() {
+                return channel_width;
+            }
         }
 
-        if vht_channel_widths == ChannelWidth::TwentyMhz | ChannelWidth::FortyMhz {
-            return ChannelWidth::FortyMhz;
+        // Check for an HE Operation element
+        if let Some(he_operation) = ies.iter().find_map(|ie| match &ie.data {
+            IeData::HeOperation(he_operation) => Some(he_operation),
+            _ => None,
+        }) {
+            if let Some(channel_width) = he_operation.channel_width() {
+                return channel_width;
+            }
         }
 
-        if vht_channel_widths
-            == ChannelWidth::EightyMhz
-                | ChannelWidth::EightyPlusEightyMhz
-                | ChannelWidth::OneSixtyMhz
-            && channel_center_segment_one == 0
-        {
-            return ChannelWidth::EightyMhz;
+        // Check for a VHT Operation element
+        if let Some(vht_operation) = ies.iter().find_map(|ie| match &ie.data {
+            IeData::VhtOperation(vht_operation) => Some(vht_operation),
+            _ => None,
+        }) {
+            if let Some(channel_width) = vht_operation.channel_width() {
+                return channel_width;
+            }
         }
 
-        if vht_channel_widths
-            == ChannelWidth::EightyMhz
-                | ChannelWidth::EightyPlusEightyMhz
-                | ChannelWidth::OneSixtyMhz
-            && channel_center_segment_one > 0
-            && (channel_center_segment_one - channel_center_segment_zero) == 8
-        {
-            return ChannelWidth::OneSixtyMhz;
+        // Check for an HT Operation element
+        if let Some(ht_operation) = ies.iter().find_map(|ie| match &ie.data {
+            IeData::HtOperation(ht_operation) => Some(ht_operation),
+            _ => None,
+        }) {
+            match ht_operation.ht_operation_information.sta_channel_width {
+                SupportedChannelWidths::TwentyMhz => return ChannelWidth::TwentyMhz,
+                SupportedChannelWidths::Any => return ChannelWidth::FortyMhz,
+            }
         }
 
-        if vht_channel_widths.0
-            == ChannelWidth::EightyMhz
-                | ChannelWidth::EightyPlusEightyMhz
-                | ChannelWidth::OneSixtyMhz
-            && channel_center_segment_one > 0
-            && (channel_center_segment_one - channel_center_segment_zero) > 16
-        {
-            return ChannelWidth::EightyPlusEightyMhz;
-        }
-
-        if vht_channel_widths == ChannelWidths::from(ChannelWidth::EightyPlusEightyMhz) {
-            return ChannelWidth::EightyPlusEightyMhz;
-        }
-
-        if vht_channel_widths == ChannelWidths::from(ChannelWidth::OneSixtyMhz) {
-            return ChannelWidth::OneSixtyMhz;
-        }
-
-        ChannelWidth::TwentyMhz
+        // If there is no EHT/HE/VHT/HT Information
+        ChannelWidth::TwentyMhzNoHt
     }
 }
 
 impl Display for ChannelWidth {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            ChannelWidth::TwentyMhzNoHt => write!(f, "20 MHz, non-HT"),
             ChannelWidth::TwentyMhz => write!(f, "20 MHz"),
-            ChannelWidth::TwentyTwoMhz => write!(f, "22 MHz"),
             ChannelWidth::FortyMhz => write!(f, "40 MHz"),
             ChannelWidth::EightyMhz => write!(f, "80 MHz"),
             ChannelWidth::EightyPlusEightyMhz => write!(f, "80+80 MHz"),
             ChannelWidth::OneSixtyMhz => write!(f, "160 MHz"),
+            ChannelWidth::FiveMhzOfdm => write!(f, "5 MHz OFDM"),
+            ChannelWidth::TenMhzOfdm => write!(f, "10 MHz OFDM"),
+            ChannelWidth::OneMhzOfdm => write!(f, "1 MHz OFDM"),
+            ChannelWidth::TwoMhzOfdm => write!(f, "2 MHz OFDM"),
+            ChannelWidth::FourMhzOfdm => write!(f, "4 MHz OFDM"),
+            ChannelWidth::EightMhzOfdm => write!(f, "8 MHz OFDM"),
+            ChannelWidth::SixteenMhzOfdm => write!(f, "16 MHz OFDM"),
+            ChannelWidth::ThreeHundredTwentyMhz => write!(f, "320 MHz"),
         }
     }
 }
 
-#[derive(
-    Debug,
-    Copy,
-    Clone,
-    PartialEq,
-    Eq,
-    Deref,
-    DerefMut,
-    BitAnd,
-    BitAndAssign,
-    BitOr,
-    BitOrAssign,
-    BitXor,
-    BitXorAssign,
-    From,
-    Not,
-)]
+#[derive(Debug, Clone, PartialEq, Eq, Deref, DerefMut, From)]
 #[from(forward)]
-pub struct ChannelWidths(BitFlags<ChannelWidth>);
-
-impl PartialEq<BitFlags<ChannelWidth, u8>> for ChannelWidths {
-    fn eq(&self, other: &BitFlags<ChannelWidth, u8>) -> bool {
-        self.0.eq(other)
-    }
-}
+pub struct ChannelWidths(HashSet<ChannelWidth>);
 
 impl Display for ChannelWidths {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut channel_widths = Vec::new();
-
-        for channel_width in [
-            ChannelWidth::TwentyMhz,
-            ChannelWidth::TwentyTwoMhz,
-            ChannelWidth::FortyMhz,
-            ChannelWidth::EightyMhz,
-            ChannelWidth::EightyPlusEightyMhz,
-            ChannelWidth::OneSixtyMhz,
-        ]
-        .iter()
-        {
-            if self.contains(*channel_width) {
-                channel_widths.push(channel_width.to_string());
-            }
-        }
-
-        write!(f, "{}", channel_widths.join(", "))
+        write!(
+            f,
+            "{}",
+            self.0
+                .iter()
+                .map(|channel_width| channel_width.to_string())
+                .collect::<Vec<String>>()
+                .join(", ")
+        )
     }
 }
