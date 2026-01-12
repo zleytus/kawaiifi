f0039dfacee7f112d9edd54a838747f4fda150e8
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-01-12 17:20:36 -0500

    Add `nl80211::ParseError` for nl80211 parsing

---
+++ lib/src/nl80211/error.rs
@@ -0,0 +1,19 @@
+use thiserror::Error;
+
+#[derive(Debug, Clone, Error)]
+pub(crate) enum ParseError {
+    #[error("Missing required attribute: {0}")]
+    MissingAttribute(&'static str),
+
+    #[error("Unexpected command type: expected {expected:?}, got {got:?}")]
+    UnexpectedCommand {
+        expected: super::Cmd,
+        got: super::Cmd,
+    },
+
+    #[error("Neli deserialization error: {0}")]
+    DeError(#[from] neli::err::DeError),
+
+    #[error("Deku parsing error: {0}")]
+    Deku(#[from] deku::error::DekuError),
+}
--- lib/src/nl80211/mod.rs
+++ lib/src/nl80211/mod.rs
@@ -1,11 +1,13 @@
 mod attr;
 mod bss;
 mod cmd;
+mod error;
 mod types;
 
 pub(crate) use attr::Attr;
 pub(crate) use bss::Bss;
 pub(crate) use cmd::Cmd;
+pub(crate) use error::ParseError;
 pub(crate) use types::*;
 
 pub(crate) const NL80211_FAMILY_NAME: &str = "nl80211";
