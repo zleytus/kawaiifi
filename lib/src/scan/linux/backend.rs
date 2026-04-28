8b0aaa89d9af2b694ebb067ef172f65a32c6ca1a
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-04-28 15:05:16 -0500

    Add doc comments to `Backend`

--- lib/src/scan/linux/backend.rs
+++ lib/src/scan/linux/backend.rs
@@ -1,5 +1,8 @@
+/// The backend used to perform Wi-Fi scans on Linux.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Backend {
+    /// Scan via nl80211, the Linux kernel's native Wi-Fi netlink interface.
     Nl80211,
+    /// Scan via NetworkManager's D-Bus API.
     NetworkManager,
 }
