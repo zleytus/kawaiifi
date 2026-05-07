88ae1ea75f0146e24e1cb4b72566f7c0a33eac4f
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-05-07 12:30:17 -0500

    Add site info (WIP)

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  image: {
+    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
+  },
 });
