f71dc779505a49b9e41e6a7a837699c13e68b3cc
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-05-15 14:39:44 -0500

    Fix clippy warnings in `ffi` crate

--- ffi/build.rs
+++ ffi/build.rs
@@ -6,27 +6,34 @@ fn main() {
 fn generate_c_bindings() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let mut config = cbindgen::Config::default();
-    config.language = cbindgen::Language::C;
-    config.documentation = true;
-    config.include_guard = Some("kawaiifi_h".to_string());
-    config.enumeration = cbindgen::EnumConfig {
-        rename_variants: cbindgen::RenameRule::ScreamingSnakeCase,
-        prefix_with_name: true,
-        ..Default::default()
-    };
-    config.parse = cbindgen::ParseConfig {
-        parse_deps: true,
-        include: Some(vec!["kawaiifi".to_string()]),
-        ..Default::default()
-    };
-    config.export = cbindgen::ExportConfig {
-        item_types: vec![
-            cbindgen::ItemType::Functions,
-            cbindgen::ItemType::Enums,
-            cbindgen::ItemType::Structs,
-        ],
-        exclude: vec!["GUID".to_string()],
+    let mut config = cbindgen::Config {
+        language: cbindgen::Language::C,
+        documentation: true,
+        include_guard: Some("kawaiifi_h".to_string()),
+        enumeration: cbindgen::EnumConfig {
+            rename_variants: cbindgen::RenameRule::ScreamingSnakeCase,
+            prefix_with_name: true,
+            ..Default::default()
+        },
+        parse: cbindgen::ParseConfig {
+            parse_deps: true,
+            include: Some(vec!["kawaiifi".to_string()]),
+            ..Default::default()
+        },
+        export: cbindgen::ExportConfig {
+            item_types: vec![
+                cbindgen::ItemType::Functions,
+                cbindgen::ItemType::Enums,
+                cbindgen::ItemType::Structs,
+            ],
+            exclude: vec!["GUID".to_string()],
+            ..Default::default()
+        },
+        after_includes: Some(
+            "#ifdef _WIN32\n#include <guiddef.h>\n#endif\ntypedef struct Interface Interface;\ntypedef struct Bss Bss;\ntypedef struct Scan Scan;\ntypedef struct Ie Ie;\ntypedef struct FieldList FieldList;\ntypedef struct InterfaceList InterfaceList;\ntypedef struct BssList BssList;"
+                .to_string(),
+        ),
+        cpp_compat: true,
         ..Default::default()
     };
     config
@@ -35,13 +42,6 @@ fn generate_c_bindings() {
     config
         .defines
         .insert("target_os = windows".to_string(), "_WIN32".to_string());
-    config.after_includes = Some(
-    "#ifdef _WIN32\n#include <guiddef.h>\n#endif\ntypedef struct Interface Interface;\ntypedef struct Bss Bss;\ntypedef struct Scan Scan;\ntypedef struct Ie Ie;\ntypedef struct FieldList FieldList;\ntypedef struct InterfaceList InterfaceList;\ntypedef struct BssList BssList;"
-    .to_string(),
-    );
-
-    config.cpp_compat = true;
-
     cbindgen::generate_with_config(crate_dir, config)
         .expect("Unable to generate bindings")
         .write_to_file("include/kawaiifi.h");
--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,3 +1,15 @@
+//! C-compatible FFI bindings for `kawaiifi`.
+//!
+//! # Safety
+//!
+//! Unless a function says otherwise, pointer arguments may be null only when
+//! represented as nullable parameters in the generated C header. Borrowed
+//! pointers returned from this library remain valid only for the lifetime of
+//! their owning object. Owned strings and byte buffers returned from this
+//! library must be released with the matching `kawaiifi_*_free` function.
+
+#![allow(clippy::missing_safety_doc)]
+
 mod bss;
 mod common;
 pub mod field;
