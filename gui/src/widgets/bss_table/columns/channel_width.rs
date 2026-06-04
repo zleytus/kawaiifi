1b89ace06d460aa4f329657f32657df2abfa9ad0
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-06-04 16:32:17 -0500

    Use `ChannelWidth::Display` impl in `BssTable`

--- gui/src/widgets/bss_table/columns/channel_width.rs
+++ gui/src/widgets/bss_table/columns/channel_width.rs
@@ -1,19 +1,10 @@
 use gtk::SignalListItemFactory;
-use kawaiifi::ChannelWidth;
 
 use super::{create_bss_sorter_by, create_bss_text_factory};
 
 pub fn create_channel_width_factory() -> SignalListItemFactory {
     create_bss_text_factory(gtk::Align::End, |bss| {
-        let width = match bss.channel_width() {
-            ChannelWidth::TwentyMhz => "20",
-            ChannelWidth::FortyMhz => "40",
-            ChannelWidth::EightyMhz => "80",
-            ChannelWidth::EightyPlusEightyMhz => "80+80",
-            ChannelWidth::OneSixtyMhz => "160",
-            ChannelWidth::ThreeHundredTwentyMhz => "320",
-        };
-        Some(format!("{width} MHz"))
+        Some(bss.channel_width().to_string())
     })
 }
 
