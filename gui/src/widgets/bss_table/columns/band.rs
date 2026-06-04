198820203d4616917ac7729868cfd7518323c35a
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-06-04 16:31:11 -0500

    Use `Band::Display` impl in `BssTable`

--- gui/src/widgets/bss_table/columns/band.rs
+++ gui/src/widgets/bss_table/columns/band.rs
@@ -1,17 +1,9 @@
 use gtk::SignalListItemFactory;
-use kawaiifi::Band;
 
 use super::{create_bss_sorter_by, create_bss_text_factory};
 
 pub fn create_band_factory() -> SignalListItemFactory {
-    create_bss_text_factory(gtk::Align::End, |bss| {
-        let band = match bss.band() {
-            Band::TwoPointFourGhz => "2.4",
-            Band::FiveGhz => "5",
-            Band::SixGhz => "6",
-        };
-        Some(format!("{band} GHz"))
-    })
+    create_bss_text_factory(gtk::Align::End, |bss| Some(bss.band().to_string()))
 }
 
 pub fn create_band_sorter() -> gtk::CustomSorter {
