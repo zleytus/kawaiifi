241e8cff064221b4f216e7333ac9ea9e26f71219
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-06-04 16:32:55 -0500

    Use `create_bss_sorter_by` helper for security col

--- gui/src/widgets/bss_table/columns/security.rs
+++ gui/src/widgets/bss_table/columns/security.rs
@@ -1,17 +1,11 @@
 use gtk::SignalListItemFactory;
-use gtk::prelude::*;
 
-use super::create_bss_text_factory;
-use crate::objects::BssObject;
+use super::{create_bss_sorter_by, create_bss_text_factory};
 
 pub fn create_security_factory() -> SignalListItemFactory {
     create_bss_text_factory(gtk::Align::Start, |bss| Some(bss.security().to_string()))
 }
 
 pub fn create_security_sorter() -> gtk::CustomSorter {
-    gtk::CustomSorter::new(|obj1, obj2| {
-        let bss1 = obj1.downcast_ref::<BssObject>().unwrap();
-        let bss2 = obj2.downcast_ref::<BssObject>().unwrap();
-        bss1.security().cmp(&bss2.security()).into()
-    })
+    create_bss_sorter_by(|bss| *bss.security())
 }
