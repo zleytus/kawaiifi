319cb09a08c817bb6e5fdbdfb3c97ce73fce7c6e
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-06-09 16:30:49 -0500

    Make `scan.c` consistent with .NET/Rust examples

--- ffi/examples/scan.c
+++ ffi/examples/scan.c
@@ -19,7 +19,24 @@ int main() {
     int64_t scan_duration_ms = kawaiifi_scan_end_time_utc_ms(scan) -
                                kawaiifi_scan_start_time_utc_ms(scan);
 
-    printf("Found %zu BSS(s) in %" PRId64 " ms\n", bss_count, scan_duration_ms);
+    #if defined(__linux__)
+    uintptr_t freqs_count = 0;
+    kawaiifi_scan_freqs_mhz(scan, &freqs_count);
+    char *name = kawaiifi_interface_name(interface);
+    printf("Found %zu BSS(s) in %" PRId64 " ms on %zu frequencies using %s\n",
+           bss_count, scan_duration_ms, freqs_count, name);
+    kawaiifi_string_free(name);
+    #elif defined(__APPLE__)
+    char *name = kawaiifi_interface_name(interface);
+    printf("Found %zu BSS(s) in %" PRId64 " ms using %s\n", bss_count,
+           scan_duration_ms, name);
+    kawaiifi_string_free(name);
+    #elif defined(_WIN32)
+    char *description = kawaiifi_interface_description(interface);
+    printf("Found %zu BSS(s) in %" PRId64 " ms using %s\n", bss_count,
+           scan_duration_ms, description);
+    kawaiifi_string_free(description);
+    #endif
 
     kawaiifi_scan_free(scan);
     kawaiifi_interface_free(interface);
