d5128f578335dd4be67cf940102fd8ca457f5c74
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-06-12 18:08:28 -0500

    Fix formatting in `channel_width.rs`

--- gui/src/widgets/bss_table/columns/channel_width.rs
+++ gui/src/widgets/bss_table/columns/channel_width.rs
@@ -3,9 +3,7 @@ use gtk::SignalListItemFactory;
 use super::{create_bss_sorter_by, create_bss_text_factory};
 
 pub fn create_channel_width_factory() -> SignalListItemFactory {
-    create_bss_text_factory(gtk::Align::End, |bss| {
-        Some(bss.channel_width().to_string())
-    })
+    create_bss_text_factory(gtk::Align::End, |bss| Some(bss.channel_width().to_string()))
 }
 
 pub fn create_channel_width_sorter() -> gtk::CustomSorter {
