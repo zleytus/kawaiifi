73bcbea531dd1186a2f31b08ef794fb694a3a7a3
Zach Leytus <22868934+zleytus@users.noreply.github.com>
2026-06-17 14:54:28 -0500

    Add initial support for packaging with snap

--- gui/src/config.rs
+++ gui/src/config.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 pub const APP_ID: Option<&str> = option_env!("APP_ID");
 pub const RESOURCES_FILE: Option<&str> = option_env!("RESOURCES_FILE");
 
@@ -5,6 +7,14 @@ pub fn app_id() -> &'static str {
     APP_ID.expect("APP_ID env var not set at compile time")
 }
 
-pub fn resources_file() -> &'static str {
-    RESOURCES_FILE.expect("RESOURCES_FILE env var not set at compile time")
+pub fn resources_file() -> PathBuf {
+    let resources_file = RESOURCES_FILE.expect("RESOURCES_FILE env var not set at compile time");
+
+    if let Some(snap) = std::env::var_os("SNAP")
+        && resources_file.starts_with('/')
+    {
+        return PathBuf::from(snap).join(resources_file.trim_start_matches('/'));
+    }
+
+    PathBuf::from(resources_file)
 }
